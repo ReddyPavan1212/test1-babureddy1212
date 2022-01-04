a67094e92ae6921aa389cdb4d13c30c6c3c5f329
ReddyPavan1212 <94963098+ReddyPavan1212@users.noreply.github.com>
2022-01-04 09:27:34 +0530

    Update p7final.c

---
+++ p7final.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  int num,i,sum=0;
+  printf("enter the number\n");
+  scanf("%d",&sum);
+  for(i=1;i<=num;i++)
+  sum=sum+i;
+  printf("the sum of n numbers = %d",sum);
+  return 0;
+}
